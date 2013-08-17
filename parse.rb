f64dac6cd33e04c263caec4d08cc7a33408be2cf
Kevin Marsh <kevin.marsh@gmail.com>
2013-08-17 09:10:30 -0400

    Initial commit

---
+++ parse.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'oj'
+require 'pp'
+
+h2 = Oj.load(ARGF)
+h2['features'].each do |feature|
+
+  # ["STATEFP",
+  #  "COUNTYFP",
+  #  "TLID",
+  #  "TFIDL",
+  #  "TFIDR",
+  #  "MTFCC",
+  #  "FULLNAME",
+  #  "SMID",
+  #  "LFROMADD",
+  #  "LTOADD",
+
+  puts "#{feature['properties']['FULLNAME']}|#{feature['properties']['RFROMADD']}|#{feature['properties']['RTOADD']}|#{feature['properties']['LFROMADD']}|#{feature['properties']['LTOADD']}|#{feature['geometry']['coordinates']}"
+end
