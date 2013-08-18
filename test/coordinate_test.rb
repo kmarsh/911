9dc0842af5069c36f36df51a20e71bf811c31297
Kevin Marsh <kevin.marsh@gmail.com>
2013-08-18 13:15:34 -0400

    Cleanup test

--- test/coordinate_test.rb
+++ test/coordinate_test.rb
@@ -3,13 +3,17 @@ require "minitest/autorun"
 require "address_extractor"
 
 class TestAddressMatch < Minitest::Test
-  def test_distance_to
+  def test_distance_to_with_small_distances
     starr_and_sheldon = Coordinate.new(-83.500316, 41.643731)
     starr_and_dearborn = Coordinate.new(-83.501809, 41.643709)
 
-    assert_in_delta 0.1241, starr_and_sheldon.distance_to(starr_and_dearborn)
-    assert_in_delta 0.0771, starr_and_sheldon.distance_to(starr_and_dearborn, :mi)
-    assert_in_delta 407.152, starr_and_sheldon.distance_to(starr_and_dearborn, :ft), 1.0
-    assert_in_delta 124.1, starr_and_sheldon.distance_to(starr_and_dearborn, :m), 1.0
+    # Manually verified via http://itouchmap.com/?p=sheldon+and+starr+toledo+ohio&submit=Show+Map&t=l&r=measuredistance
+    assert_in_delta 0.125, starr_and_sheldon.distance_to(starr_and_dearborn) # km
+    assert_in_delta 0.078, starr_and_sheldon.distance_to(starr_and_dearborn, :mi)
+    assert_in_delta 410,   starr_and_sheldon.distance_to(starr_and_dearborn, :ft), 10.0
+    assert_in_delta 125.0, starr_and_sheldon.distance_to(starr_and_dearborn, :m), 1.0
+
+    old_house = Coordinate.new(-83.500686, 41.644590)
+    new_house = Coordinate.new(-83.583029, 41.703493)
   end
 end
