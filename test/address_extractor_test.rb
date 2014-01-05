67c014bccc3e16badc0175995d36817de5ff2d35
Kevin Marsh <kevin.marsh@gmail.com>
2014-01-05 16:51:40 -0500

    Formatting for specs

--- test/address_extractor_test.rb
+++ test/address_extractor_test.rb
@@ -24,17 +24,17 @@ class TestAddressExtractor < Minitest::Test
   end
 
   def test_with_intersection
-    assert_equal IntersectionMatch.new("arch st", "hayden st"), AddressExtractor.new("TPD/TFD - injuries from an assault, arch st and hayden st").extract
-    # assert_equal IntersectionMatch.new("hill ave", "wenz"), AddressExtractor.new("#ScannerOn -TPD - multiple callers reporting gunshots heard in the area, hill ave and wenz").extract
-    assert_equal IntersectionMatch.new("country creek ln", "wenz rd"), AddressExtractor.new("TPD - crew had group that fired shots, country creek ln and wenz rd, running south towards airport hwy, crews requested code 3").extract
+    assert_equal IntersectionMatch.new("arch st", "hayden st"),                 AddressExtractor.new("TPD/TFD - injuries from an assault, arch st and hayden st").extract
+    assert_equal IntersectionMatch.new("hill ave", "wenz"),                     AddressExtractor.new("#ScannerOn -TPD - multiple callers reporting gunshots heard in the area, hill ave and wenz").extract
+    assert_equal IntersectionMatch.new("country creek ln", "wenz rd"),          AddressExtractor.new("TPD - crew had group that fired shots, country creek ln and wenz rd, running south towards airport hwy, crews requested code 3").extract
     assert_equal IntersectionMatch.new("airport hwy", "s holland sylvania rd"), AddressExtractor.new("TPD - 8 sector crews to a code 3 injury accident, airport hwy and s holland sylvania rd, reported car into a pole").extract
-    assert_equal IntersectionMatch.new("n detroit ave", "palmwood ave"), AddressExtractor.new("TPD - crews at n detroit ave and palmwood ave not finding anybody shot on that corner, other crews slowing down").extract
-    assert_equal IntersectionMatch.new("e manhattan blvd", "lagrange st"), AddressExtractor.new("TPD - attempted robbery, meet the papa johhs pizza driver at e manhattan blvd and lagrange st, driver pepper sprayed two potential robbers").extract
-    assert_equal IntersectionMatch.new("kent st", "page st"), AddressExtractor.new("TPD - security reporting group of unwanted males from apartments kent st and page st, heading out towards cherry st").extract
-    assert_equal IntersectionMatch.new("water st", "n summit st"), AddressExtractor.new("do see that toledo fire and ems dispatched water rescue for water st and n summit st, owens corning area, sounds like downtown suspect").extract
-    assert_equal IntersectionMatch.new("spencer st", "south ave"), AddressExtractor.new("TPD - a few gang unit crews out on a suspect stop at spencer st and south ave").extract
-    assert_equal IntersectionMatch.new("birmingham terrace", "consaul st"), AddressExtractor.new("TPD - disorder, birmingham terrace near consaul st, report of 5-6 people arguing").extract
-    assert_equal IntersectionMatch.new("riverside ct", "summit st"), AddressExtractor.new("TPD/TFD/EMS - possible overdose, riverside ct near summit st").extract
+    assert_equal IntersectionMatch.new("n detroit ave", "palmwood ave"),        AddressExtractor.new("TPD - crews at n detroit ave and palmwood ave not finding anybody shot on that corner, other crews slowing down").extract
+    assert_equal IntersectionMatch.new("e manhattan blvd", "lagrange st"),      AddressExtractor.new("TPD - attempted robbery, meet the papa johhs pizza driver at e manhattan blvd and lagrange st, driver pepper sprayed two potential robbers").extract
+    assert_equal IntersectionMatch.new("kent st", "page st"),                   AddressExtractor.new("TPD - security reporting group of unwanted males from apartments kent st and page st, heading out towards cherry st").extract
+    assert_equal IntersectionMatch.new("water st", "n summit st"),              AddressExtractor.new("do see that toledo fire and ems dispatched water rescue for water st and n summit st, owens corning area, sounds like downtown suspect").extract
+    assert_equal IntersectionMatch.new("spencer st", "south ave"),              AddressExtractor.new("TPD - a few gang unit crews out on a suspect stop at spencer st and south ave").extract
+    assert_equal IntersectionMatch.new("birmingham terrace", "consaul st"),     AddressExtractor.new("TPD - disorder, birmingham terrace near consaul st, report of 5-6 people arguing").extract
+    assert_equal IntersectionMatch.new("riverside ct", "summit st"),            AddressExtractor.new("TPD/TFD/EMS - possible overdose, riverside ct near summit st").extract
   end
 
   def test_null_match
